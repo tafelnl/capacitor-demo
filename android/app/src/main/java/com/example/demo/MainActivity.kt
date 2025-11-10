595022a160ed0f40a4cbeca036ed4aee63b01ef9
tafelnl <35837839+tafelnl@users.noreply.github.com>
2025-11-10 16:03:31 +0100

    feat: add safe area plugin

--- android/app/src/main/java/com/example/demo/MainActivity.kt
+++ android/app/src/main/java/com/example/demo/MainActivity.kt
@@ -1,5 +1,14 @@
 package com.example.demo
 
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
 import com.getcapacitor.BridgeActivity
+import com.getcapacitor.community.safearea.SafeAreaPlugin
 
-class MainActivity : BridgeActivity()
+class MainActivity : BridgeActivity() {
+    public override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        SafeAreaPlugin.setSystemBarsStyle(this, SafeAreaPlugin.SystemBarsStyle.LIGHT)
+    }
+}
